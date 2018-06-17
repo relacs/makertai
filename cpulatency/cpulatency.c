3647fae7351939873923ddf391fd6f047497174c
Jan Benda <jan.benda@uni-tuebingen.de>
2018-06-18 01:36:58 +0200

    [cpulatency] check availability of pm qos functions

--- cpulatency/cpulatency.c
+++ cpulatency/cpulatency.c
@@ -5,32 +5,39 @@
 #include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/cpu.h>
-#include <linux/pm_qos.h>
+#include <linux/pm_qos.h>   // since 3.2
 
 MODULE_LICENSE( "GPL" );
 MODULE_DESCRIPTION( "Add a PM QoS request for zero latency of a specific CPU" );
 MODULE_AUTHOR( "Jan Benda <jan.benda@uni-tuebingen.de>" );
 
-// MODULE_SUPPORTED_DEVICE("cpu_dma_latency"); ???
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,0,0)
-//  extern unsigned long cpu_isolated_map; // I do not find this in the kernel headers
+/* The id of the CPU on which the latency should be limited to zero.
+   If -1 then take the first isolated CPU.
+   If -2 then apply zero latency to all CPUs. */
+static int cpu_id = -2;
+module_param( cpu_id, int, S_IRUSR | S_IRGRP | S_IROTH );
+MODULE_PARM_DESC( cpu_id, "Id of CPU on which latencies should be set to zero" );
+
+
+/*
+since 2.6.28: cpumask_var_t with cpumask_test_cpu( i, cpu_isolated_map )
+until 2.6.28: cpumask_t with cpu_isset( i, cpu_isolated_map )
+before 4.1: cpu_isolated_map not extern! Try compiling RTAI with a pre 4.0 kernel!!!
+ */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,1,0)
+//  extern unsigned long cpu_isolated_map;
 #else
   extern cpumask_var_t cpu_isolated_map;
 #endif
 int isolatedCPUId = -1;
 
-/* The id of the CPU on which the resume latency should be limited.
-   If -1 then take the first isolated CPU.
-   If -2 then apply zero latency to all CPUs. */
-static int cpu_id = -2;
-
-module_param( cpu_id, int, S_IRUSR | S_IRGRP | S_IROTH );
-MODULE_PARM_DESC( cpu_id, "Id of CPU on which latencies should be set to zero" );
-
 
 static struct pm_qos_request cpu_dma_latency_req;
-static struct dev_pm_qos_request cpu_resume_latency_req;
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,8,0)
+static struct dev_pm_qos_request cpu_resume_latency_req;  // since 3.2
+#endif
 
 
 static int __init init_cpulatency( void )
@@ -38,14 +45,11 @@ static int __init init_cpulatency( void )
   int i;
   int latency = 0;
 
-  printk( KERN_INFO "INIT CPULATENCY\n" );
-
   if ( cpu_id == -1 ) {
     // find first isolated cpu:
 #ifdef CONFIG_SMP
     for ( i=0; i<NR_CPUS; i++ ) {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,0,0)
-      //      if ( cpu_isolated_map & (1<<i) ) {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,1,0)
       if ( 0 ) {
 #else
       if ( cpumask_test_cpu( i, cpu_isolated_map ) ) {
@@ -58,32 +62,44 @@ static int __init init_cpulatency( void )
     if ( isolatedCPUId >= 0 )
       cpu_id = isolatedCPUId;
     else
-      printk( KERN_INFO "no isolated CPU - set latency for all CPUs to zero\n");
+      printk( KERN_INFO "cpulatency: no isolated CPU\n");
   }
 
+#ifndef CONFIG_PM
+    /*  Device specific PM QoS only implemented for CONFIG_PM enabled:
+	"Device power management core functionality" */
+    cpu_id = -2;
+#elseif LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0)
+    cpu_id = -2;
+#endif
+
   if ( cpu_id < 0 ) {
-    /* Set maximum CPU latency to zero for all CPUs: */
-    /* This is equivalent to what the /dev/cpu_dma_latency file does. */
     memset( &cpu_dma_latency_req, 0, sizeof cpu_dma_latency_req );
-    printk( KERN_INFO "cpulatency: setting latency of all CPUs to zero\n" );
     pm_qos_add_request( &cpu_dma_latency_req, PM_QOS_CPU_DMA_LATENCY, latency );
+    printk( KERN_INFO "cpulatency: set latency of all CPUs to zero\n" );
+    /* This is equivalent to what the /dev/cpu_dma_latency file does! */
   }
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,8,0)
+#ifdef CONFIG_PM
   else {
     struct device *cpudev = get_cpu_device( cpu_id );
     if ( cpudev != NULL ) {
-      printk( KERN_INFO "cpulatency: setting latency for CPU %d to zero\n", cpu_id );
-      // only implemented for CONFIG_PM kernel parameter!
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,0,0)
+      memset( &cpu_resume_latency_req, 0, sizeof cpu_resume_latency_req );
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,15,0)
       dev_pm_qos_add_request( cpudev, &cpu_resume_latency_req, DEV_PM_QOS_LATENCY, latency );
 #else
       dev_pm_qos_add_request( cpudev, &cpu_resume_latency_req, DEV_PM_QOS_RESUME_LATENCY, latency );
 #endif
+      printk( KERN_INFO "cpulatency: set latency for CPU %d to zero\n", cpu_id );
     }
     else {
-      printk( KERN_INFO "Invalid CPU id %d\n", cpu_id );
+      printk( KERN_INFO "cpulatency: invalid CPU id %d\n", cpu_id );
       return -EINVAL;
     }
   }
+#endif
+#endif
 
   return 0;
 }
@@ -91,14 +107,15 @@ static int __init init_cpulatency( void )
 
 static void __exit cleanup_cpulatency( void )
 {
-  printk(KERN_INFO "cpulatency: removing PM QoS request\n");
-
   if ( cpu_id < 0 )
     pm_qos_remove_request( &cpu_dma_latency_req );
+
+#ifdef CONFIG_PM
   else
     dev_pm_qos_remove_request( &cpu_resume_latency_req );
-    
-  printk( KERN_INFO "EXIT CPULATENCY\n" );
+#endif
+
+  printk( KERN_INFO "cpulatency: removed all power management requests on CPUs\n" );
 }
 
 module_init( init_cpulatency );
